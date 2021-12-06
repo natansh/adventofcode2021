86601cf9ac2a63274c197095d0368ef0ef9d48bb
natansh <natansh@gmail.com>
2021-12-05 18:32:48 -0800

    [day5][part 1] Hydrothermal vent

--- day5.py
+++ day5.py
@@ -1,35 +1,40 @@
-import regex as re
+from parse import parse
 
-with open("data/day5.txt") as file:
-    point_occ = dict()
-    for line in file.readlines():
-        line = line.rstrip()
-        match = re.search('(\d+),(\d+) -> (\d+),(\d+)', line)
-        x1, y1, x2, y2 = (int(match.group(1)), int(match.group(2)), int(match.group(3)), int(match.group(4)))
-        if x1 == x2:
-            if y1 > y2:
-                temp = y1
-                y1 = y2
-                y2 = temp
-            for y in range(y1, y2 + 1):
-                if (x1, y) in point_occ:
-                    point_occ[(x1, y)] += 1
-                else:
-                    point_occ[(x1, y)] = 1
-        elif y1 == y2:
-            if x1 > x2:
-                temp = x1
-                x1 = x2
-                x2 = temp
-            for x in range(x1, x2 + 1):
-                if (x, y1) in point_occ:
-                    point_occ[(x, y1)] += 1
-                else:
-                    point_occ[(x, y1)] = 1    
+def increment_count(a1, a2):
+    if a1 == a2:
+        return 0
+    elif a1 < a2:
+        return 1
+    else:
+        return -1
 
+def overlap_point_count(point_occ):
     count = 0
     for key, value in point_occ.items():
         if value > 1:
             count += 1
+    return count
 
-    print(count)
+with open("data/day5.txt") as file:
+    point_occ_a = dict()
+    point_occ_b = dict()
+    for line in file.readlines():
+        line = line.rstrip()
+        x1, y1, x2, y2 = parse("{:d},{:d} -> {:d},{:d}", line)
+        incr_x = increment_count(x1, x2)
+        incr_y = increment_count(y1, y2)
+        count = max(abs(y2 - y1), abs(x2 - x1))
+        
+        for i in range(count + 1):
+            x = x1 + incr_x * i
+            y = y1 + incr_y * i
+            # Part A
+            if incr_x == 0 or incr_y == 0:
+                point_occ_a[(x, y)] = point_occ_a.get((x, y), 0) + 1
+            # Part B
+            point_occ_b[(x, y)] = point_occ_b.get((x, y), 0) + 1
+
+    print("Day 5 - a")
+    print(overlap_point_count(point_occ_a))
+    print("Day 5 - b")
+    print(overlap_point_count(point_occ_b))
